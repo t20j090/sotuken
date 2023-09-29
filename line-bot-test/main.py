aeac4de8c71880bd823bf5892efcee71e11ca34d
t20j090 <114897740+t20j090@users.noreply.github.com>
2023-09-29 14:37:40 +0900

    Add files via upload

--- line-bot-test/main.py
+++ line-bot-test/main.py
@@ -18,7 +18,8 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-categories = ['国内', '国際', '経済', 'エンタメ', 'スポーツ', 'IT', '科学', 'ライフ']
+yahoo_categories = ['国内', '国際', '経済', 'エンタメ', 'スポーツ', 'IT', '科学', 'ライフ']
+cnn_categories = ['World', 'USA', 'Business', 'Tech', 'Entertainment', 'Odd News']
 news_sites = ['ヤフーニュース', 'CNN']
 
 # 0:ヤフーニュース 1:CNNニュース
@@ -58,7 +59,7 @@ def yahoo_news_ranking():
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     #クラス名が変わるので注意！！
-    elements = soup.select('.sc-dvXYtj.iduHXF')
+    elements = soup.select('.sc-fXazdy.UjHkE')
     elements = elements[:5]
     
     news_urls = []
@@ -93,8 +94,8 @@ def send_news_ranking():
 now = datetime.datetime.now().time()
 
 # 判定する時間帯を設定（開始時刻・終了時刻から-9時間する必要がある）
-start_time = datetime.time(9, 50)  # 開始時刻 9:50
-end_time = datetime.time(10, 10)   # 終了時刻 10:10
+start_time = datetime.time(21, 50)  # 開始時刻 21:50
+end_time = datetime.time(22, 10)   # 終了時刻 22:10
 
 #現在の日付を取得
 # t_delta = datetime.timedelta(hours=9)
@@ -102,17 +103,18 @@ end_time = datetime.time(10, 10)   # 終了時刻 10:10
 # now = datetime.datetime.now(JST)
 # current_time = now.strftime('%Y/%m/%d %H:%M:%S')
 
+#LineのユーザーID
 user_id = 'U2925b78f74c0cbde9804be066eb707f0'
 
 # 判定条件
 if (start_time <= now <= end_time):
    send_news_ranking()
-else:
-  line_bot_api.push_message(user_id, TextSendMessage(text='送信できません'))
+#else:
+   #line_bot_api.push_message(user_id, TextSendMessage(text='送信できません'))
 
         
-#ニュースの情報を取得
-def scraping(topic):
+#ヤフーニュースの情報を取得
+def yahoo_news_scraping(topic):
     url = 'https://news.yahoo.co.jp/'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
@@ -139,7 +141,7 @@ def scraping(topic):
     r = requests.get(URL)
     soup = BeautifulSoup(r.text, "html.parser")
     #クラス名が変わるので注意！！
-    elements = soup.select('.sc-dvXYtj.iduHXF')
+    elements = soup.select('.sc-fXazdy.UjHkE')
     
     elements = elements[:5]
     
@@ -151,15 +153,14 @@ def scraping(topic):
         title = element.text[1:]
         news_urls.append(url)
         news_titles.append(title)
-        time.sleep(0.5)
-    
+            
     news_text = []
     #ニュースの本文を取得
     for news_url in news_urls:
         r = requests.get(news_url)
         soup = BeautifulSoup(r.text, "html.parser")
         #クラス名が変わるので注意！！ 
-        elements = soup.select('.sc-gDyJDg.dmrAcv.yjSlinkDirectlink.highLightSearchTarget')
+        elements = soup.select('.sc-iMCRTP.ePfheF.yjSlinkDirectlink.highLightSearchTarget')
     
         text = ""
     
@@ -176,7 +177,7 @@ def scraping(topic):
         #ページが1つだけのとき、エラーが出るため
         try:
             #クラス名が変わるので注意！！ （取得例：２ページ)
-            page_element = soup.select('.sc-ihRHuF.kFnlxQ')
+            page_element = soup.select('.sc-cpUASM.bqNXll')
             page = page_element[0].text         
             page_str = ''.join(filter(str.isdigit, page))
             page_num = int(page_str)
@@ -190,7 +191,7 @@ def scraping(topic):
                 r = requests.get(news_url_page)
                 soup = BeautifulSoup(r.text, "html.parser")
                 #クラス名が変わるので注意！！ 
-                elements = soup.select('.sc-gDyJDg.dmrAcv.yjSlinkDirectlink.highLightSearchTarget')
+                elements = soup.select('.sc-iMCRTP.ePfheF.yjSlinkDirectlink.highLightSearchTargett')
                 #ニュース記事の本文が配列で区切られていた場合
                 for i in range(0, 8):
                     try:
@@ -213,6 +214,83 @@ def scraping(topic):
 
     return articles
 
+#CNNニュースの情報を取得
+def cnn_news_scraping(topic):
+    url = 'https://www.cnn.co.jp'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    
+    #クラス名が変わるので注意！！
+    elements = soup.select('.pg-container li a')
+    elements = elements[6:11] + [elements[14], elements[16]]
+    
+    topic = topic
+    #トピックのURL取得
+    for i, element in enumerate(elements):
+        if topic in element.text:
+            categories_num = i
+    
+    element = elements[categories_num]
+    
+    topic_url = element['href']
+    URL = url + topic_url
+    
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, "html.parser")
+    
+    #クラス名が変わるので注意！！
+    elements = soup.select('.cd a')
+    
+    elements = elements[:3]
+    
+    news_urls = []
+    news_titles = []
+    #ニュースのURLとタイトルを取得
+    for element in elements:
+        news_url =  url + element['href']
+        news_title = element.text
+    
+        news_urls.append(news_url)
+        news_titles.append(news_title)
+    
+    news_text = []
+    #ニュースの本文を取得
+    for i, news_url in enumerate(news_urls):
+        text = ""
+        
+        for j in range(1, 7):
+            num = str(j)
+            page = '-' + num + '.html'    
+            # 文字列の置換を行い新しいURLを生成
+            news_url_page = news_urls[i].replace('.html', page)
+    
+            #ページ数が複数のとき、ページ遷移し本文を取得
+            response = requests.get(news_url_page)
+    
+            # レスポンスのステータスコードを確認
+            if response.status_code != 404: 
+                r = requests.get(news_url_page)
+                soup = BeautifulSoup(r.text, "html.parser")
+                #クラス名が変わるので注意！！ 
+                elements = soup.find(id = 'leaf-body')
+                text += elements.text
+            else:
+                continue
+    
+        news_text.append(text)
+
+    articles = []
+    for url, title, text in zip(news_urls, news_titles, news_text):
+        article = {
+            "title" : title,
+            "URL" : url,
+            "text" : text
+        }
+        articles.append(article)
+
+    return articles
+
+#データをきれいに整える
 def clean_data(articles):
     for i in range(len(articles)):
         articles[i]['title'] = articles[i]['title'].replace('\u3000', '') 
@@ -231,23 +309,34 @@ def handle_message(event):
     #     event.reply_token,
     #     TextMessage(text=user_id))
     
-    cnn_news_ranking()
+    global news_flag
     
     if (event.message.text == 'ヤフーニュース'):
         news_flag = 0
     elif (event.message.text == 'CNN'):
         news_flag = 1
     
-    if event.message.text in categories:
-        articles_dict = scraping(event.message.text)
-        articles = clean_data(articles_dict)
-    elif (event.message.text in news_sites):
-        pass
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='国内, 国際, 経済, エンタメ, スポーツ, IT, 科学, ライフを入力してください。'))
-    
+    if (news_flag == 0):
+        if event.message.text in yahoo_categories:
+            articles_dict = yahoo_news_scraping(event.message.text)
+            articles = clean_data(articles_dict)
+        elif (event.message.text in news_sites):
+            pass
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='国内, 国際, 経済, エンタメ, スポーツ, IT, 科学, ライフのいずれかを入力してください。'))
+    elif (news_flag == 1):
+        if (event.message.text in cnn_categories):
+            articles_dict = cnn_news_scraping(event.message.text)
+            articles = clean_data(articles_dict)
+        elif (event.message.text in news_sites):
+            pass
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='World, USA, Business, Tech, Entertainment, Odd Newsのいずれかを入力してください。'))
+        
     article_urls = []
     for i, article in enumerate(articles):
         article_urls.append(articles[i]['URL'])
